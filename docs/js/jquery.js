56807a909d4608093b00baabaf5aecfb8c431abd
Shahaed Hasan <shahaed77@gmail.com>
2018-12-23 23:54:58 -0600

    Changed from vanilla js to jquery cause you know...it's 2018

---
+++ docs/js/jquery.js
@@ -0,0 +1,12 @@
+function main() {
+    $(".navbar-burger").click(function() {
+        $(".navbar-burger").toggleClass("is-active");
+        $(".navbar-menu").slideToggle(400);
+    });
+
+    $('.collapsible-message-header').on('click', function (){
+      $(this).next().slideToggle(400);
+      $(this).toggleClass('active');
+    });
+  }
+$(document).ready(main);
