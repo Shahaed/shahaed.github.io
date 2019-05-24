d13e101425fe722c90255c1647f4f5920030de67
Shahaed Hasan <shahaed77@gmail.com>
2019-05-23 22:23:49 -0500

    WIP: Webgl

--- docs/js/jquery.js
+++ docs/js/jquery.js
@@ -1,12 +1,14 @@
 function main() {
-    $(".navbar-burger").click(function() {
-        $(".navbar-burger").toggleClass("is-active");
-        $(".navbar-menu").slideToggle(400);
-    });
+  $(".navbar-burger").click(function() {
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").slideToggle(400);
+  });
 
-    $('.collapsible-message-header').on('click', function (){
-      $(this).next().slideToggle(400);
-      $(this).toggleClass('active');
-    });
-  }
+  $(".collapsible-message-header").on("click", function() {
+    $(this)
+      .next()
+      .slideToggle(400);
+    $(this).toggleClass("active");
+  });
+}
 $(document).ready(main);
